8bd219e788e843910eeb3d815e80c849e814d4f1
Topper Bowers <topper@toppingdesign.com>
2021-01-03 21:41:10 +0100

    fix package, remove dist

--- packages/kasumah-multicall/dist/hardhat.config.d.ts
+++
@@ -1,6 +0,0 @@
-import "@nomiclabs/hardhat-waffle";
-import "@nomiclabs/hardhat-ethers";
-import { HardhatUserConfig } from "hardhat/config";
-import 'hardhat-deploy';
-declare const config: HardhatUserConfig;
-export default config;
--- packages/kasumah-multicall/dist/types/ethers-contracts/index.d.ts
+++
@@ -1,4 +0,0 @@
-export type { Multicall } from "./Multicall";
-export type { Echo } from "./Echo";
-export { Multicall__factory } from "./factories/Multicall__factory";
-export { Echo__factory } from "./factories/Echo__factory";
