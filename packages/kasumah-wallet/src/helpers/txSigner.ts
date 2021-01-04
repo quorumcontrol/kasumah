3b2bda3ed0ffdbbdf3eb355e04c32fc5df1ed00c
Topper Bowers <topper@toppingdesign.com>
2021-01-04 14:29:58 +0100

    passing test for gnosis

---
+++ packages/kasumah-wallet/src/helpers/txSigner.ts
@@ -0,0 +1,58 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+import { BigNumber, BigNumberish, BytesLike, Signer } from "ethers"
+
+type Address = string
+
+export enum OPERATION {
+    CALL = 0,
+    CREATE = 2,
+}
+
+export const signer = async function(signer:SignerWithAddress, verifyingContract:Address, to:Address, value:BigNumberish, data:BytesLike, operation:OPERATION, txGasEstimate:BigNumberish, baseGasEstimate:BigNumberish, gasPrice:BigNumberish, txGasToken:Address, refundReceiver:Address, nonce:BigNumberish) {
+    if (!BigNumber.isBigNumber(value)) {
+        value = BigNumber.from(value)
+    }
+    let typedData = {
+        types: {
+            EIP712Domain: [
+                { type: "address", name: "verifyingContract" }
+            ],
+            // "SafeTx(address to,uint256 value,bytes data,uint8 operation,uint256 safeTxGas,uint256 baseGas,uint256 gasPrice,address gasToken,address refundReceiver,uint256 nonce)"
+            SafeTx: [
+                { type: "address", name: "to" },
+                { type: "uint256", name: "value" },
+                { type: "bytes", name: "data" },
+                { type: "uint8", name: "operation" },
+                { type: "uint256", name: "safeTxGas" },
+                { type: "uint256", name: "baseGas" },
+                { type: "uint256", name: "gasPrice" },
+                { type: "address", name: "gasToken" },
+                { type: "address", name: "refundReceiver" },
+                { type: "uint256", name: "nonce" },
+            ]
+        },
+        domain: {
+            verifyingContract
+        },
+        primaryType: "SafeTx",
+        message: {
+            to: to,
+            value: value.toHexString(),
+            data: data,
+            operation: operation,
+            safeTxGas: txGasEstimate,
+            baseGas: baseGasEstimate,
+            gasPrice: gasPrice,
+            gasToken: txGasToken,
+            refundReceiver: refundReceiver,
+            nonce: BigNumber.from(nonce).toNumber()
+        }
+    }
+    console.log('after typed data')
+
+
+    let sig = await (signer.provider as any).send("eth_signTypedData",[signer.address,typedData]);
+
+    
+    return sig
+}
--- packages/kasumah-wallet/test/Wallet.ts
+++ packages/kasumah-wallet/test/Wallet.ts
@@ -1,29 +1,72 @@
 import { ethers, network } from "hardhat";
 import { expect } from "chai";
 import { deployCanonicals } from "../src/helpers/deploy";
-import { GnosisSafe__factory } from "../types/ethers-contracts";
+import { GnosisSafeProxy__factory, GnosisSafeProxyFactory, GnosisSafeProxyFactory__factory, GnosisSafe__factory } from "../types/ethers-contracts";
 import { GnosisSafe } from '../types/ethers-contracts/GnosisSafe'
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+import { OPERATION, signer } from "../src/helpers/txSigner";
+import { BigNumber, constants, utils, Wallet } from "ethers";
 
 const addr0 = ethers.constants.AddressZero
 
 describe("MulticallWrapper", () => {
-  let gnosisSafe:GnosisSafe
+  let masterCopy:GnosisSafe
+  let gnosisSafeFactory:GnosisSafe__factory
+  let proxyFactory:GnosisSafeProxyFactory
+
   let deployer:SignerWithAddress
+  let alice:SignerWithAddress
 
   before(async () => {
     const signers = await ethers.getSigners()
     deployer = signers[0]
+    alice = signers[1]
     await deployCanonicals(ethers.provider, deployer)
-    const gsf = new GnosisSafe__factory(deployer)
+    gnosisSafeFactory = new GnosisSafe__factory(deployer)
     // const gs = new GnosisSafe('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', gsf.interface, deployer)
-    gnosisSafe = gsf.attach('0x6851D6fDFAfD08c0295C392436245E5bc78B0185')
+    masterCopy = gnosisSafeFactory.attach('0x6851D6fDFAfD08c0295C392436245E5bc78B0185')
+
+    const proxyFactoryFactory = new GnosisSafeProxyFactory__factory(deployer)
+    proxyFactory = proxyFactoryFactory.attach('0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B')
   });
 
   it("sanity works", async () => {
+    const setupData = await masterCopy.populateTransaction.setup([deployer.address], 1, addr0, '0x', addr0, addr0, 0, addr0)
+    if (!setupData.data) {
+        throw new Error("no setup data")
+    }
+    const walletTx = await proxyFactory.createProxyWithNonce(masterCopy.address, setupData.data, 1)
+    const walletReceipt = await walletTx.wait()
 
+    const userWallet = gnosisSafeFactory.attach(walletReceipt.events![0].args?.proxy)
+    console.log('userWallet: ', userWallet.addres)
 
-    console.log(await gnosisSafe.populateTransaction.setup([deployer.address], 1, addr0, '0x', addr0, addr0, 0, addr0))
+    console.log('sending ', (await deployer.getBalance()).toString())
+    const sendTx = await deployer.sendTransaction({
+        to: userWallet.address,
+        from: deployer.address,
+        value: utils.parseEther('1').toHexString(),
+        gasLimit: 50000,
+    })
+    console.log('sent')
+
+    const halfEth = utils.parseEther('0.5')
+
+    const sig = await signer(deployer, userWallet.address, alice.address, halfEth, '0x', OPERATION.CALL, 0, 0, 0, constants.AddressZero, constants.AddressZero, 0)
+    console.log('sig: ', sig.length)
+    await userWallet.execTransaction(
+        alice.address, 
+        halfEth, 
+        '0x', 
+        OPERATION.CALL, 
+        0, 
+        0, 
+        0, 
+        constants.AddressZero, 
+        constants.AddressZero,
+        sig
+    )
+    expect(await deployer.provider?.getBalance(userWallet.address)).to.equal(halfEth)
   });
 
 });
